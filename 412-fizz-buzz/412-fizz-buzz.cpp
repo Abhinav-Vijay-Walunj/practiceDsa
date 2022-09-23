dc0dcfa7073cff243daad47e56dd3ff842549eec
Abhinav-Vijay-Walunj <64826301+Abhinav-Vijay-Walunj@users.noreply.github.com>
2022-09-23 22:21:13 +0530

    Time: 10 ms (15.61%), Space: 8 MB (34.27%) - LeetHub

---
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector <string> vec;
+        string ch;
+        string s;
+        for(int i=1;i<=n;i++)
+        {
+            // ch=i;
+            // s=ch[0];
+            if(i%3 == 0 && i%5 == 0)
+                vec.push_back("FizzBuzz");
+            else if(i%3 == 0)
+                vec.push_back("Fizz");
+            else if(i%5 == 0)
+                vec.push_back("Buzz");
+            else
+            {
+                
+                vec.push_back(to_string(i));
+            }
+                
+            
+                
+        }
+        return vec;
+    }
+};
